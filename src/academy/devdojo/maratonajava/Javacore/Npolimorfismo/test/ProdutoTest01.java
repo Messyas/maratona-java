01164c3391afe3a7a5c6db30b2e4d9f85d1d8452
Messyas <messyasgoisfranca@gmail.com>
2024-02-12 10:27:39 -0400

    Polimofirsmo - Parametros polimorficos - Superclasse como variavel de referencia e subclasse como Objeto

--- src/academy/devdojo/maratonajava/Javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/Javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.Javacore.Npolimorfismo.test;
 
 import academy.devdojo.maratonajava.Javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.Javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.Javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.Javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,10 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("Nuc10I7", 11000);
         Tomate tomate = new Tomate("Tomate calvo", 20);
+        Televisao televisao = new Televisao("Samsung 50\" ",5000);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("--------------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
-
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("--------------------------------");
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
