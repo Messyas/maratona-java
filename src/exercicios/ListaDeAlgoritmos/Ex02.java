0ce417d1dc9722427fdf0fe81c882491fded1575
Messyas <messyasgoisfranca@gmail.com>
2024-02-19 11:54:29 -0400

    Lista de exercicio de algoritmos - Produto dos numeros inteiros no intervalo de 0 a 100

--- src/exercicios/ListaDeAlgoritmos/Ex02.java
+++ src/exercicios/ListaDeAlgoritmos/Ex02.java
@@ -1,4 +1,25 @@
 package exercicios.ListaDeAlgoritmos;
 
+/**
+ * Exibir o produto dos números inteiros positivos no intervalo de um a cem.
+ */
 public class Ex02 {
+    public static void main(String[] args) {
+        int contagem = 100;
+        int numero = 0;
+        int produtoInteiros = 0;
+        int proxNum = 0;
+        int produtoDoIntervaloDezeroACem=0;
+        while(numero<contagem) {
+            numero++;
+            proxNum++;
+            produtoInteiros = (numero*proxNum);
+            System.out.println("o numero" + numero + " X " + proxNum + " é igual a " + produtoInteiros);
+            produtoDoIntervaloDezeroACem += produtoInteiros;
+
+        }
+        System.out.println("O produto dos numeros inteiros é " + produtoDoIntervaloDezeroACem);
+
+    }
+
 }
