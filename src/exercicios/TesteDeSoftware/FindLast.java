bd804d51219f1e8e5a3407b9938a96b687a8d594
Messyas <messyasgoisfranca@gmail.com>
2024-02-29 22:29:10 -0400

    Teste da classe FindLast

--- src/exercicios/TesteDeSoftware/FindLast.java
+++ src/exercicios/TesteDeSoftware/FindLast.java
@@ -4,6 +4,9 @@ package exercicios.TesteDeSoftware;
 // Authors: Paul Ammann & Jeff Offutt
 // Chapter 1, section 1.2, page 16
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class FindLast {
 
     public static int findLast(int[] x, int y) {  //Effects: If x==null throw NullPointerException
@@ -12,7 +15,7 @@ class FindLast {
         //   If no such element exists, return -1
 
         // As the example in the book points out, this loop should end at 0.
-        for (int i = x.length - 1; i > 0; i--) {
+        for (int i = x.length - 1; i >= 0; i--) {
             if (x[i] == y) {
                 return i;
             }
@@ -21,11 +24,4 @@ class FindLast {
     }
     // test:  x=[2, 3, 5]; y = 2
     //        Expected = 0
-    public static void main() {
-        int[] x = {2,3,5};
-        int y = 2;
-        int retorno = findLast(x, y);
-        System.out.println(retorno);
-    }
-
 }
---
+++ src/exercicios/TesteDeSoftware/FindLastTest.java
@@ -0,0 +1,26 @@
+package exercicios.TesteDeSoftware;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FindLastTest {
+
+    private int[] array;
+    private int valueToFind;
+
+    @BeforeEach
+    void setUp() {
+        array = new int[] {2,3,5};
+        valueToFind = 2;
+    }
+
+    @Test
+    void testFindLast() {
+        int expectedIndex = 0;
+        int actualIndex = FindLast.findLast(array,valueToFind);
+        assertEquals(expectedIndex, actualIndex, "Expected index does not match actual index");
+    }
+}
