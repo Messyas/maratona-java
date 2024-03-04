5404e78faa8b3b181e466dc702e1f49aa23ceeeb
Messyas <messyasgoisfranca@gmail.com>
2024-03-03 21:24:22 -0400

    como usar o static outros exemplos

--- src/exercicios/ExerciciosDevDojo/ExerComoUsarStatic.java
+++ src/exercicios/ExerciciosDevDojo/ExerComoUsarStatic.java
@@ -3,6 +3,15 @@ package exercicios.ExerciciosDevDojo;
 public class ExerComoUsarStatic {
     //variavel estatica
     public static int contador = 0;//o valor é modificado para qualquer instancia da classe
+    //Exemplo de uso com constantes
+    /**\
+     * public class Constantes {
+     *     public static final double PI = 3.14159; // o final faz com que o valor nao possa ser alterado em tempo de execucao
+     *     public static final int NUMERO_MAXIMO_DE_CLIENTES = 100;
+     * }
+     */
+//casos de uso
+
     //metodo estatico
     public static void incremetarContador() {
         contador++;
@@ -12,6 +21,53 @@ public class ExerComoUsarStatic {
     public void exibirContardor() {
         System.out.println("Contador " + contador);
     }
+
+    /**
+     * Contadores e Rastreadores Globais: Quando você precisa rastrear informações globais sobre todas as instâncias de uma
+     * classe, como um contador de instâncias, é útil usar membros estáticos.
+     *
+     * public class Contador {
+     *     private static int contadorDeInstancias = 0;
+     *
+     *     public Contador() {
+     *         contadorDeInstancias++;
+     *     }
+     *
+     *     public static int getContadorDeInstancias() {
+     *         return contadorDeInstancias;
+     *     }
+     * }
+     */
+
+    /**
+     *Métodos Utilitários: Se você tem métodos que não dependem do estado de instância da classe e podem ser compartilhados
+     * entre todas as instâncias, é aconselhável torná-los estáticos. Por exemplo:
+     */
+    //exemplos de uso
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static double calcularAreaDoCirculo(double raio) {
+        return Math.PI * raio * raio;
+    }
+
+    /**
+     * Padrão Singleton: Quando você precisa garantir que uma classe tenha apenas uma única instância e quer
+     * fornecer um ponto de acesso global a essa instância, pode usar membros estáticos para implementar o padrão Singleton.
+     *
+     * public class Singleton {
+     *     private static Singleton instancia = new Singleton();
+     *
+     *     private Singleton() {
+     *         // Construtor privado para evitar instanciação externa
+     *     }
+     *
+     *     public static Singleton getInstancia() {
+     *         return instancia;
+     *     }
+     * }
+     */
 
     public static void main(String[] args) {
         //acessando membros estaticos diremente
