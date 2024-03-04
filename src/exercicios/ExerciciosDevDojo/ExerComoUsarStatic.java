d914a68cfe59db2977edf68ccd66b53153e0243d
Messyas <messyasgoisfranca@gmail.com>
2024-03-03 21:19:38 -0400

    como usar o static

--- src/exercicios/ExerciciosDevDojo/ExerComoUsarStatic.java
+++ src/exercicios/ExerciciosDevDojo/ExerComoUsarStatic.java
@@ -1,7 +1,28 @@
 package exercicios.ExerciciosDevDojo;
 
 public class ExerComoUsarStatic {
-    public static void main(String[] args) {
-
+    //variavel estatica
+    public static int contador = 0;//o valor é modificado para qualquer instancia da classe
+    //metodo estatico
+    public static void incremetarContador() {
+        contador++;
     }
+
+    //metodo nao estatico
+    public void exibirContardor() {
+        System.out.println("Contador " + contador);
+    }
+
+    public static void main(String[] args) {
+        //acessando membros estaticos diremente
+        ExerComoUsarStatic.incremetarContador();// a classe executa a funcao para a variavel em si mesma, ou seja o statica faz com que os atributos passem a pertencer a classe e nao a objetos
+        ExerComoUsarStatic.incremetarContador();
+
+        ExerComoUsarStatic object01 = new ExerComoUsarStatic();
+        object01.exibirContardor();
+
+        ExerComoUsarStatic object02 = new ExerComoUsarStatic();
+        object02.exibirContardor();
+    }
+
 }
