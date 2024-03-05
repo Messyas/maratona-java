14c1501559ba58ceba341ffbc92ea613937d84a1
Messyas <messyasgoisfranca@gmail.com>
2024-03-04 21:50:32 -0400

    Classes patos: Padrao de projetos Strategy

---
+++ src/exercicios/Abilio/PP/Patos/Pato.java
@@ -0,0 +1,14 @@
+package exercicios.Abilio.PP.Patos;
+
+public abstract class Pato {
+
+   public Pato() {
+
+   }
+
+   public void display() {
+       System.out.println("Display: Mostra patos de todas as categorias");
+   }
+
+
+}
